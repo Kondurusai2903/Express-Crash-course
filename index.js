6b33e2e89ef979a1c76776588a3a7b1b20f7ca21
konduru sai <kondurusai863@gmail.com>
2024-08-18 13:02:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require("express");
-// const path = require("path");
-// const userposts = require("./routers/posts");
-// const logger = require("./middleware/logger");
-const blogRouter = require("./routers/blog.router");
 const app = express();
-const PORT = 3001;
 const connectDB = require("./db/config");
+const PORT = 5000;
+const blogRouter = require("./routes/blog.routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// setup static folder;
-app.use(express.static(path.join(__dirname, "public")));
-// app.use("/blogs", blogRouter);
-
-app.listen(PORT, () => console.log(`Server is listening....${PORT}`));
-
-// --------------body parser middleware
-// app.use(logger);
-// app.use("/api/posts", userposts);
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-// app.get("/about", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "about.html"));
-// });
+app.use("/blogs", blogRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
